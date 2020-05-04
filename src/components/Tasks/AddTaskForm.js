4a6f68f26b2564047b83c05d0c793b78a191591a
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2020-05-04 18:46:01 +0400

    React Connected Router. Made deleting and editing.

--- src/components/Tasks/AddTaskForm.js
+++ src/components/Tasks/AddTaskForm.js
@@ -27,7 +27,7 @@ const AddTaskForm = ({ list, onAddTask }) => {
         onAddTask(list.id, data)
         toggleFormVisible()
       })
-      .catch(() => {
+      .catch(e => {
         alert('Ошибка при добавлении задачи!')
       })
       .finally(() => {
